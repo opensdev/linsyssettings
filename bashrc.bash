8ab0f63008ab9af8564e524fd17d2337ca39e75d
Shivamurthy Shastri <shivamurthy.shastri@opensdev.com>
2019-03-16 04:38:53 +0100

    bash: add bash configs for ctags, cscope
    Signed-off-by: Shivamurthy Shastri <shivamurthy.shastri@opensdev.com>

--- bashrc.bash
+++ bashrc.bash
@@ -1,8 +1,21 @@
-#Append content of this file to .bashrc
-if [ -f /etc/bash_completion ]; then
-        . /etc/bash_completion
-fi
+# function to set terminal title
+function set-title(){
+  if [[ -z "$ORIG" ]]; then
+    ORIG="$PS1"
+  fi
+  TITLE="\[\e]2;$*\a\]"
+  PS1="${ORIG}${TITLE}"
+}
+
+# generate the tags and cscope
+function ta(){
+	#clean older info
+	rm -rf tags
+	rm -rf cscope.files
+	rm -rf cscope.out
+	#generate new info
+	find $PWD | egrep -i "\.(c|h|cpp)$" > cscope.files
+	catgs -R . *.c *.h *.cpp --tag-relative=yes ./
+}
 
 PS1='\[\e[33;1m\]\u@\h: \[\e[31m\]\W\[\e[0m\]\$ '
-source ~/.git-completion.bash
-
